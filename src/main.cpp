5492aaf7deaac283039d84d04a9ed7d2960537ff
Abraham Fernandez <claudiayabrahammusic@gmail.com>
2023-06-17 20:19:18 -0400

    Creating saves files

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@ using std::cin;
 using std::endl;
 
 #include <fstream>
+using std::fstream;
 using std::ofstream;
 using std::ifstream;
 using std::ios;
@@ -21,18 +22,25 @@ using std::ios;
 
 int main(){
     //Building the castle
+
+    /*
     Room library, armor, music;
     library.setchest(1).setenemies(4).setname("Library");
     armor.setchest(2).setenemies(7).setname("Armor hall");
     music.setchest(1).setenemies(5).setname("Music hall");
     //Construidos
+    */
+
+
 
     //Creating Banya
     Banya banya;
 
     //Variables to mecanics
+    /*
     int option=0, move=0, menu=0, progress[4]={0,0,0,0}, lose=0;
     banya.setdamage(1000);
+    */
     
 
 
